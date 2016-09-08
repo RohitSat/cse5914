8805902152fbb25bbc6402f1a2dcfed3ca776181
Alexandru Barbur <root.ctrlc@gmail.com>
2016-09-08 18:47:36 -0400

    Fix another word swap error.

--- vagrant/web.sh
+++ vagrant/web.sh
@@ -7,11 +7,11 @@ virtualenv --python=/usr/bin/python3 /home/vagrant/env
 # install python requirements in the virtualenv
 source /home/vagrant/env/bin/activate
 pip install -r /vagrant/src/brutus-api/requirements.txt
-pip install -r /vagrant/src/brutus-math-module/requirements.txt
+pip install -r /vagrant/src/brutus-module-math/requirements.txt
 
 # install the web app in-place (you can update the code without reinstalling)
 pip install -e /vagrant/src/brutus-api
-pip install -e /vagrant/src/brutus-math-module
+pip install -e /vagrant/src/brutus-module-math
 
 # fix virtualenv permissions (because we're running as root)
 chown -R vagrant:vagrant /home/vagrant/env
