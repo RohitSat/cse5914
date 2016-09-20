34a144eeb9077ceb258cbc0f373d740f12e75de5
Alexandru Barbur <root.ctrlc@gmail.com>
2016-09-20 18:02:06 -0400

    Fix the module unit tests to use the simple request-response HTTP API.

--- src/brutus-module-math/brutus_module_math/tests/basic_test.py
+++ src/brutus-module-math/brutus_module_math/tests/basic_test.py
@@ -10,18 +10,26 @@ class BasicTestCase(BrutusTestCase):
 
     def test_create_request(self):
         """
-        Create a new request and verify it contains an ID and the
-        provided input data.
+        Create a new request and verify it contains the
+        provided input data and some output data.
         """
 
         # create the request
         request_data = {'text': 'what is 1 plus 1'}
-        create_data, create_response = self.parse_response(
+        api_data, api_response = self.parse_response(
             self.app.post(self.BRUTUS_API_REQUEST, data=request_data))
 
         # verify the request contains the input data
-        assert hasattr(create_data, 'input')
-        input_data = create_data['input']
+        assert hasattr(api_data, 'input')
+        input_data = api_data['input']
 
         assert isinstance(input_data, dict)
         assert input_data == request_data
+
+        # verify the request contains output data
+        assert hasattr(api_data, 'output')
+        output_data = api_data['output']
+
+        assert isinstance(output_data, dict)
+        assert hasattr(output_data, 'text')
+        assert isinstance(output_data['text'], str)
