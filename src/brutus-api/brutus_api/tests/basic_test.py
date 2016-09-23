977d96e1cc4aec0d31ff6544540433c33a8eeebd
Alexandru Barbur <root.ctrlc@gmail.com>
2016-09-23 01:11:57 -0400

    Fix unit tests for the brutus-api project. We can run background tasks and properly mock requests without socket shenanigans.

--- src/brutus-api/brutus_api/tests/basic_test.py
+++ src/brutus-api/brutus_api/tests/basic_test.py
@@ -23,36 +23,36 @@ class BasicTestCase(BrutusTestCase):
 
         # create the request
         request_data = {'text': 'what is 1 plus 1'}
-        create_data, create_response = self.parse_response(self.app.post(
+        create_data, _ = self.parse_response(self.client.post(
             self.BRUTUS_API_REQUEST,
             data=json.dumps(request_data),
             content_type='application/json'))
 
         # verify the request contains a numeric id
-        assert hasattr(create_data, 'id')
+        assert 'id' in create_data
         request_id = create_data['id']
 
         assert isinstance(request_id, str)
 
         # verify the request contains the input data
-        assert hasattr(create_data, 'input')
+        assert 'input' in create_data
         input_data = create_data['input']
 
         assert isinstance(input_data, dict)
         assert input_data == request_data
 
         # retrieve the request
-        retrieve_data, retrieve_response = self.parse_response(
-            self.app.get(self.BRUTUS_API_REQUEST_BY_ID.format(id=request_id)))
+        retrieve_data, _ = self.parse_response(self.client.get(
+            self.BRUTUS_API_REQUEST_BY_ID.format(id=request_id)))
 
         # verify the request contains a numeric id
-        assert hasattr(retrieve_data, 'id')
+        assert 'id' in create_data
         request_id = retrieve_data['id']
 
         assert isinstance(request_id, str)
 
         # verify the request contains the input data
-        assert hasattr(retrieve_data, 'input')
+        assert 'input' in create_data
         input_data = retrieve_data['input']
 
         assert isinstance(input_data, dict)
@@ -71,7 +71,7 @@ class BasicTestCase(BrutusTestCase):
         for i in range(self.REQUEST_SAMPLE_SIZE):
             # create the request
             request_data = {'text': 'what is 1 plus 1'}
-            create_data, create_response = self.parse_response(self.app.post(
+            create_data, _ = self.parse_response(self.client.post(
                 self.BRUTUS_API_REQUEST,
                 data=json.dumps(request_data),
                 content_type='application/json'))
@@ -82,3 +82,33 @@ class BasicTestCase(BrutusTestCase):
 
         # verify we have a unique id for each request we created
         assert len(request_ids) == self.REQUEST_SAMPLE_SIZE
+
+    def test_module_request(self):
+        """
+        Create a request and verify the backend queries the correct module.
+        """
+
+        # register module URLs with generic data
+        self.register_common_urls()
+
+        # create the request
+        request_data = {'text': 'what is 1 plus 1'}
+        create_data, create_response = self.parse_response(self.client.post(
+            self.BRUTUS_API_REQUEST,
+            data=json.dumps(request_data),
+            content_type='application/json'))
+
+        # process background tasks
+        self.process_jobs()
+
+        # retrieve the request
+        request_id = create_data['id']
+        retrieve_data, _ = self.parse_response(self.client.get(
+            self.BRUTUS_API_REQUEST_BY_ID.format(id=request_id)))
+
+        # verify the output
+        assert retrieve_data == {
+            'id': request_id,
+            'input': request_data,
+            'output': {'text': '2'},
+            'status': 'finished'}
