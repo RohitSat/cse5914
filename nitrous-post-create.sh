3af4e0c90876e7f92f13600f6db6efa25afbaac3
Alexandru Barbur <root.ctrlc@gmail.com>
2016-09-27 00:02:21 +0000

    Setup nitrous quick start files. [ciskip]

---
+++ nitrous-post-create.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# settings
+VENV="${HOME}/env"
+REPO="${HOME}/code/cse5914"
+
+# install redis
+sudo add-apt-repository -y ppa:chris-lea/redis-server
+sudo apt-get update
+sudo apt-get -y install redis-server
+
+# create a virtualenv for the application
+apt-get -y install python-virtualenv python3-dev
+virtualenv --python=/usr/bin/python3 "${VENV}"
+
+# install python requirements in the virtualenv
+source "${VENV}/bin/activate"
+pip install -r "${REPO}/src/brutus-api/requirements.txt"
+pip install -r "${REPO}/src/brutus-module-math/requirements.txt"
+pip install -r "${REPO}/src/brutus-module-weather/requirements.txt"
+
+# install the web app in-place (you can update the code without reinstalling)
+pip install -e "${REPO}/src/brutus-api"
+pip install -e "${REPO}/src/brutus-module-math"
+pip install -e "${REPO}/src/brutus-module-weather"
+
+# configure the vagrant user's profile
+echo "source ${REPO}/bin/activate" >> "${HOME}/.profile"
