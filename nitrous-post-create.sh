c2ff35202d0ae32a5402184b26747238c64eac1b
Alexandru Barbur <root.ctrlc@gmail.com>
2016-09-29 14:02:23 -0400

    Update nitrous-post-create.sh again to properly set environment language. [ciskip]

--- nitrous-post-create.sh
+++ nitrous-post-create.sh
@@ -3,7 +3,6 @@
 # settings
 VENV="${HOME}/env"
 REPO="${HOME}/code/cse5914"
-PIP_OPTS="--ignore-installed"
 
 # install ubuntu package tools
 sudo apt-get -y install software-properties-common
@@ -12,21 +11,24 @@ sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 sudo apt-get -y install redis-server
 
+# update environment for python
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
 # create a virtualenv for the application
 sudo apt-get -y install python-virtualenv python3-dev
 virtualenv --python=/usr/bin/python3 "${VENV}"
-pip ${PIP_OPTS} install --upgrade pip setuptools
 
 # install python requirements in the virtualenv
 source "${VENV}/bin/activate"
-pip ${PIP_OPTS} install -r "${REPO}/src/brutus-api/requirements.txt"
-pip ${PIP_OPTS} install -r "${REPO}/src/brutus-module-math/requirements.txt"
-pip ${PIP_OPTS} install -r "${REPO}/src/brutus-module-weather/requirements.txt"
+pip install -r "${REPO}/src/brutus-api/requirements.txt"
+pip install -r "${REPO}/src/brutus-module-math/requirements.txt"
+pip install -r "${REPO}/src/brutus-module-weather/requirements.txt"
 
 # install the web app in-place (you can update the code without reinstalling)
-pip ${PIP_OPTS} install -e "${REPO}/src/brutus-api"
-pip ${PIP_OPTS} install -e "${REPO}/src/brutus-module-math"
-pip ${PIP_OPTS} install -e "${REPO}/src/brutus-module-weather"
+pip install -e "${REPO}/src/brutus-api"
+pip install -e "${REPO}/src/brutus-module-math"
+pip install -e "${REPO}/src/brutus-module-weather"
 
 # configure the vagrant user's profile
 echo "source ${VENV}/bin/activate" >> "${HOME}/.zprofile"
