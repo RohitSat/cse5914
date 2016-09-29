264fbe0bc1d4f55f6061f32c515d808e7b8b1bda
Alexandru Barbur <root.ctrlc@gmail.com>
2016-09-29 13:56:14 -0400

    Modify nitrous-post-create.sh to ignore pip cache. [ciskip]

--- nitrous-post-create.sh
+++ nitrous-post-create.sh
@@ -3,6 +3,7 @@
 # settings
 VENV="${HOME}/env"
 REPO="${HOME}/code/cse5914"
+PIP_OPTS="--ignore-installed --no-cache-dir"
 
 # install ubuntu package tools
 sudo apt-get -y install software-properties-common
@@ -13,21 +14,19 @@ sudo apt-get -y install redis-server
 
 # create a virtualenv for the application
 sudo apt-get -y install python-virtualenv python3-dev
-
-export LC_ALL=C
 virtualenv --python=/usr/bin/python3 "${VENV}"
-pip install --upgrade pip setuptools
+pip ${PIP_OPTS} install --upgrade pip setuptools
 
 # install python requirements in the virtualenv
 source "${VENV}/bin/activate"
-pip install -r "${REPO}/src/brutus-api/requirements.txt"
-pip install -r "${REPO}/src/brutus-module-math/requirements.txt"
-pip install -r "${REPO}/src/brutus-module-weather/requirements.txt"
+pip ${PIP_OPTS} install -r "${REPO}/src/brutus-api/requirements.txt"
+pip ${PIP_OPTS} install -r "${REPO}/src/brutus-module-math/requirements.txt"
+pip ${PIP_OPTS} install -r "${REPO}/src/brutus-module-weather/requirements.txt"
 
 # install the web app in-place (you can update the code without reinstalling)
-pip install -e "${REPO}/src/brutus-api"
-pip install -e "${REPO}/src/brutus-module-math"
-pip install -e "${REPO}/src/brutus-module-weather"
+pip ${PIP_OPTS} install -e "${REPO}/src/brutus-api"
+pip ${PIP_OPTS} install -e "${REPO}/src/brutus-module-math"
+pip ${PIP_OPTS} install -e "${REPO}/src/brutus-module-weather"
 
 # configure the vagrant user's profile
 echo "source ${VENV}/bin/activate" >> "${HOME}/.zprofile"
