cad48aad53c2fc02179581396daa083a89fd318b
sinasabet81 <sinasabet81@gmail.com>
2016-10-04 23:16:04 +0000

    scaffolding for basic speech recognition

--- src/webClient/index.js
+++ src/webClient/index.js
@@ -1,5 +1,12 @@
-const baseURL = 'http://localhost:5000';
+const baseURL = 'http://cse5914-218011.nitrousapp.com:5000';
 
+
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 const textInput = document.querySelector('#textInput');
 const submitButton = document.querySelector('#submitButton');
 const outputBox = document.querySelector('.output');
@@ -117,3 +124,19 @@ const startPolling = (jobID) => {
 
   const timeoutClear = setTimeout(poll(), timeout);
 }
+
+function startListening() {
+    recognition.start();
+    console.log('listening for input');
+}
+
+recognition.onresult = e => {
+    console.log(e);
+    console.log(e.results);
+    console.log(e.results[0][0].transcript);
+};
+
+recognition.onspeechend = () => {
+    recognition.stop();
+    console.log('no longer listening');
+};
