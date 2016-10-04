740861e64eb5f4d90fa710c80c6c533ab474647a
Alexandru Barbur <root.ctrlc@gmail.com>
2016-10-04 19:00:50 -0400

    Formatting and style fixes.

--- src/brutus-api/brutus_api/tasks.py
+++ src/brutus-api/brutus_api/tasks.py
@@ -8,11 +8,6 @@ import json
 
 from watson_developer_cloud import AuthorizationV1
 
-# TODO move to database
-baseurl = "http://127.0.0.1:"
-moduleAddresses = {'math': '5010',
-                   'weather': '5020'}
-
 
 def get_answer(text):
     """
@@ -29,27 +24,22 @@ def get_answer(text):
         db=app.config['REDIS_DB'])
 
     job = get_current_job(connection=redis)
-    # get the watson username and password
-    username = app.config['NLC_WATSON_USERNAME']
-    password = app.config['NLC_WATSON_PASSWORD']
-    classifierName = app.config['NLC_CLASSIFIER_NAME']
+
     # set up natural language processor object and pass it the classifier name
     nlc = nlp.Nlp(
-        username,
-        password,
-        classifierName)
+        app.config['NLC_WATSON_USERNAME'],
+        app.config['NLC_WATSON_PASSWORD'],
+        app.config['NLC_CLASSIFIER_NAME'])
+
     # get the module name
     module = nlc.classify(text)
-    print(text)
-    print(module)
+
     # get the result from the module
     url = baseurl + moduleAddresses[module] + "/api/request"
-    print(url)
     r = requests.post(url, json={'input': {'text': text}})
-    print(r)
-    if(r.text is None):
-        error = 'I am sorry, an error occurred'
-        return {'text': error}
+
+    if r.text is None:
+        return {'text': 'I am sorry, an error occurred'}
+
     jsonResult = json.loads(r.text)
-    print(jsonResult)
     return jsonResult['output']
