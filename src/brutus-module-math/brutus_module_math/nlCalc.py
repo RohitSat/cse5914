56a1bf5da1d777a836d03ba08089fcaccfea6633
rohit <rohitsat@ymail.com>
2016-10-11 14:35:23 -0400

    added new features to the math module

--- src/brutus-module-math/brutus_module_math/nlCalc.py
+++ src/brutus-module-math/brutus_module_math/nlCalc.py
@@ -1,76 +1,91 @@
+units = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+]
+tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+]
+scales = [
+    "hundred",
+    "thousand",
+    "million",
+    "billion",
+    "trillion"
+]
+
 def calculate(string):
+    string = fixSpecialCases(string)
     string = convertToNumbers(string)
     string = convertToSymbols(string)
+    string = fixNegatives(string)
     string = stripWords(string)
     result = evaluate(string)
     resultstring = string + ' is ' + str(result)
     return resultstring
 
+def fixSpecialCases(string):
+    #a followed by scale:
+    string=aFollowedByScale(string)
+    string=removePunctuation(string)
+
+    return string    
+
+def aFollowedByScale(string):
+    strarr=string.split()
+    for idx in xrange(len(strarr)):
+        if strarr[idx].lower()=="a":
+            if (idx+1 < len(strarr)) and (strarr[idx + 1].lower() in scales):
+                strarr[idx] = "1"
+
+    return ' '.join(strarr)
+
+def fixNegatives(string):
+    strarr=string.split()
+    newarr=[]
+    for word in strarr:
+        if word =="negative":
+                
+        
+    
+
+def removePunctuation(string):
+    #removes punctuation other than .
+    punctuationToRemove = ['?', ',' , '"', "!", "'"]
+    for punc in punctuationToRemove:
+        string =string.replace(punc, '')
+
+    return string
 
 def convertToNumbers(string):
-    oldarr = string.split()
-    newarr = []
-    for word in oldarr:
-        try:
-            newarr.append(convertWordToNumber(word))
-        except:
-            newarr.append(word)
 
-    i = 0
-    while(i < len(newarr) - 1):
-        if(newarr[i].isdigit()) and (newarr[i + 1].isdigit()):
-            cur = int(newarr[i]) + int(newarr[i + 1])
-            newarr.pop(i + 1)
-            newarr[i] = str(cur)
-        else:
-            i = i + 1
-
-    return ' '.join(newarr)
-
-
-def convertWordToNumber(string):
     numwords = {}
-    units = [
-        "zero",
-        "one",
-        "two",
-        "three",
-        "four",
-        "five",
-        "six",
-        "seven",
-        "eight",
-        "nine",
-        "ten",
-        "eleven",
-        "twelve",
-        "thirteen",
-        "fourteen",
-        "fifteen",
-        "sixteen",
-        "seventeen",
-        "eighteen",
-        "nineteen"
-    ]
-    tens = [
-        "",
-        "",
-        "twenty",
-        "thirty",
-        "forty",
-        "fifty",
-        "sixty",
-        "seventy",
-        "eighty",
-        "ninety"
-    ]
-    scales = [
-        "hundred",
-        "thousand",
-        "million",
-        "billion",
-        "trillion"
-    ]
 
     for idx, word in enumerate(units):
         numwords[word] = (1, idx)
@@ -79,17 +94,55 @@ def convertWordToNumber(string):
     for idx, word in enumerate(scales):
         numwords[word] = (10 ** (idx * 3 or 2), 0)
 
-    current = result = 0
-    for word in string.split("-"):
-        if word not in numwords:
-            raise ValueError()
-        scale, increment = numwords[word]
-        current = current * scale + increment
-        if scale > 100:
-            result += current
-            current = 0
+    oldarr = string.split()
+    newarr = []
+    idx=0
+    while(idx<len(oldarr)):
+        word=oldarr[idx]
+        if isNumber(word, numwords):
+            idx+=1
+            while(idx<len(oldarr) and isNumber(oldarr[idx], numwords)):
+                word=word+' ' + oldarr[idx]
+                idx+=1
+            result=convertWordToNumber(word, numwords)
+            newarr.append(result)
+        else:
+            idx+=1
+            newarr.append(word)
 
-    return str(result + current)
+    return ' '.join(newarr)
+
+
+def convertWordToNumber(string, numwords):
+    current = 0
+    result = 0
+    for word in string.split(" "):
+        if word in numwords: # if the word is a word
+            scale, increment = numwords[word]
+            current = current * scale + increment
+            if scale > 100:
+                result += current
+                current = 0
+        elif word.isdigit(): # if the word is digits... assumes no scales...
+            scale=1
+            increment=int(word)
+            current=current*1+ increment # Scale is assumed to be 1
+            
+        else:
+            if not isNumber(word, numwords): # Should Not happen
+                return word
+            raise ValueError("Word is neither digit nor digit")
+        
+    result=result+current
+    return str(result)
+
+def isNumber(string, numwords):
+    if string in numwords:
+        return True
+    elif string.isdigit():
+        return True
+    else:
+        return False
 
 
 def convertToSymbols(string):
@@ -129,3 +182,11 @@ def stripWords(string):
 
 def evaluate(string):
     return eval(string)
+
+import sys
+string=sys.argv[1]
+convertedstring=calculate(string)
+print convertedstring
+
+
+
