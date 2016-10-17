87fa37f953fd28e7b28d3ceac29e15251a17078c
abby <benedict.117@osu.edu>
2016-10-17 15:20:23 -0400

    Add test for more than two classifiers being returned

--- src/brutus-api/brutus_api/tests/nlc_test.py
+++ src/brutus-api/brutus_api/tests/nlc_test.py
@@ -40,10 +40,41 @@ class NlcTestCase(BrutusTestCase):
 
         assert classifier == expected_classifier
 
-    def test_get_classifier_low_confidence(self):
+    def test_get_classifier_many(self):
         """
         Test that the Nlc wrapper class returns the correct
         classifier based on the percentages it is passed
+        when it is given many classifiers
+        """
+
+        classes = [
+            {'confidence': .02, 'class_name': 'test1'},
+            {'confidence': .012, 'class_name': 'test2'},
+            {'confidence': .018, 'class_name': 'test3'},
+            {'confidence': .95, 'class_name': 'test4'}
+        ]
+        expected_classifier = 'test4'
+
+        classifier_name = self.app.config['NLC_CLASSIFIER_NAME']
+
+        # register module URLs with data
+        self.register_bluemix_url()
+        self.register_nlc_classify_url(classes)
+
+        # create Nlc class
+        nlc = Nlc(
+            'username',
+            'password',
+            classifier_name)
+
+        classifier = nlc.classify("This text doesn't actualy matter")
+
+        assert classifier == expected_classifier
+
+    def test_get_classifier_low_confidence(self):
+        """
+        Test that the Nlc wrapper class returns 'None'
+        if all of the percentages are less than 90%
         """
 
         classes = [
