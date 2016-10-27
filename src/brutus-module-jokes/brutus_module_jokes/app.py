ac45b2c12371405698cbdc3200c35c7c508ba9f2
abby <benedict.117@osu.edu>
2016-10-27 18:05:43 -0400

    Change app.py settings to match environment variables set in vagrant setup for joke module

--- src/brutus-module-jokes/brutus_module_jokes/app.py
+++ src/brutus-module-jokes/brutus_module_jokes/app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.config.update(
     DEBUG=os.getenv('DEBUG', 'False') in ['true', 'True'],
     SECRET_KEY=os.getenv('SECRET_KEY', str(uuid.uuid4())),
-    RAR_WATSON_USERNAME=os.getenv('RAR_WATSON_USERNAME'),
-    RAR_WATSON_PASSWORD=os.getenv('RAR_WATSON_PASSWORD'),
-    RAR_WATSON_CLUSTER_ID=os.getenv('RAR_WATSON_CLUSTER_ID'),
-    RAR_WATSON_COLLECTION_NAME=os.getenv('RAR_WATSON_COLLECTION_NAME'))
+    RAR_WATSON_USERNAME=os.getenv('RAR_JOKES_USERNAME'),
+    RAR_WATSON_PASSWORD=os.getenv('RAR_JOKES_PASSWORD'),
+    RAR_WATSON_CLUSTER_ID=os.getenv('RAR_JOKES_CLUSTER_ID'),
+    RAR_WATSON_COLLECTION_NAME=os.getenv('RAR_JOKES_COLLECTION_NAME'))
 
