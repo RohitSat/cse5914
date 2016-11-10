d2d96d729011d1d256145b75a699837aa8b83bfa
Abby <benedict.117@osu.edu>
2016-11-10 18:56:32 -0500

    Move work done in task into views and delete task

--- src/brutus-module-jokes/brutus_module_jokes/views.py
+++ src/brutus-module-jokes/brutus_module_jokes/views.py
@@ -1,9 +1,10 @@
+import sqlite3
 import itertools
 
 from flask import g, request, render_template, json
 
 from brutus_module_jokes import app
-from .tasks import find_joke
+from .database import connect_db, query_db
 
 
 @app.route('/')
@@ -20,9 +21,27 @@ def create_request():
     """
     Get requests or create a new request.
     """
-
     data = request.get_json()
     input_data = data['input']
-    resultstring = find_joke(input_data['text'])
-    result = {"input": input_data, 'output': {'text': resultstring}}
+    past_data = data['data']
+
+    db = connect_db(app.config['DATABASE'])
+    joke = query_db(
+            db, 
+            'SELECT * FROM jokes ORDER BY RANDOM() LIMIT 1;',
+            (),
+            single=True) 
+
+    if(request is None):
+        raise RuntimeError("joke {0} not found".format(joke_id))
+
+    jokeId = joke['id'] 
+    joke = query_db(
+            db, 
+            'SELECT * FROM jokes_parts where jokeId = ? ORDER BY id;',
+            (jokeId, ),
+            ) 
+
+    # result = {"input": input_data, 'output': {'text': j }, 'data' : { 'joke' : 'this is an example' }
+    result = {'input': input_data, 'output': {'text': j } }
     return json.jsonify(result)
